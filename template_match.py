470cdb8024ba71cb591f8121daf960da4edcd24f
Jeremy Martin <mart1358@msu.edu>
2012-06-15 15:02:14 -0400

    Matching will work with .jpg, not .png

--- template_match.py
+++ template_match.py
@@ -1,79 +1,63 @@
-from PIL import Image
-from PIL import ImageChops
+import numpy as np
 import matplotlib.pyplot as plt
+from skimage import data
+from skimage.feature import match_template
+import skimage.io as io
+from matplotlib.mlab import *
+from PIL import Image
+import matplotlib.image as mpimg
 from pylab import ginput
 
-import datetime
 
-# Crop a template to match by choosing an upper left and lower right boundary
-def template_crop(im1):
-    print "Pick the upper left and lower right corners of the template \
-to be matched"
+def template(im1):
     im = Image.open(im1)
-    pic_plt = plt.imread(im1)
+    pic_plt = mpimg.pil_to_array(im)
     y = pic_plt[:,0,0].size
-    plt.imshow(pic_plt,origin='lower')
-    LOffset = ginput(1)
-    ROffset = ginput(1)
+    fig = plt.imshow(pic_plt, origin='lower')
+    fig.axes.get_xaxis().set_visible(False)
+    fig.axes.get_yaxis().set_visible(False)
+    offset = ginput(1)
     plt.close()
-    # Have to change the column boundary due to the y-axis being flipped
-    crop_b = (int(round(LOffset[0][0])),int(round(y-LOffset[0][1])),\
-              int(round(ROffset[0][0])),int(round(y-ROffset[0][1])))
-    print 'coordinates (origin in upper left):',crop_b
-    crop_im = im.crop(crop_b)
-    crop_im.load()
-    #crop_im.save('crop.jpg')
-    return crop_im
+    Left = int(round(offset[0][0]-25))
+    Upper = int(round(offset[0][1]-25))
+    Right = int(round(offset[0][0]+25))
+    Lower = int(round(offset[0][1]+25))
+    return Left, Upper, Right, Lower
 
 '''
 Example code taken from:
-http://www.daniweb.com/software-development/python/threads/252384/python-pil-template-matching
+http://scikits-image.org/docs/dev/auto_examples/plot_template.html#example-plot-template-py
 '''
-def matchTemplate(searchImage, templateImage):
-    minScore = -1000
-    matching_xs = 0
-    matching_ys = 0
-    # convert images to "L" to reduce computation by factor 3 "RGB"->"L"
-    searchImage = searchImage.convert(mode="L")
-    templateImage = templateImage.convert(mode="L")
-    searchWidth, searchHeight = searchImage.size
-    templateWidth, templateHeight = templateImage.size
-    # make a copy of templateImage and fill with color=1
-    templateMask = Image.new(mode="L", size=templateImage.size, color=1)
-    # loop over each pixel in the search image
-    for xs in range(searchWidth-templateWidth+1):
-        for ys in range(searchHeight-templateHeight+1):
-            score = templateWidth*templateHeight
-            # crop the part from searchImage
-            searchCrop = searchImage.crop((xs,ys,xs+templateWidth,ys+templateHeight))
-            diff = ImageChops.difference(templateImage, searchCrop)
-            notequal = ImageChops.darker(diff,templateMask)
-            countnotequal = sum(notequal.getdata())
-            score -= countnotequal
+image = 'test_file'
+image = mpimg.imread(image)
+image = image[:,:,0]
+bounds = template('template_file')
+coin = image[bounds[1]:bounds[3], bounds[0]:bounds[2]]
 
-        if minScore < score:
-            minScore = score
-            matching_xs = xs
-            matching_ys = ys
+result = match_template(image, coin)
+ij = np.unravel_index(np.argmax(result), result.shape)
+x, y = ij[::-1]
 
-    print "Location=",(matching_ys, matching_xs), "Score=",minScore
-    im1 = Image.new('RGB', (searchWidth, searchHeight))
-    im1.paste(templateImage, (matching_ys, matching_xs))
-    # Saves only the matched template to the coordinates of where it was located
-    #im1.save('template_matched_in_search.png')
-    # Boundaries of matched template within the searchImage
-    print (matching_ys, matching_xs, matching_ys+templateWidth,matching_xs+templateHeight)
-    return (matching_ys, matching_xs, matching_ts+templateWidth,matching_xs+templateHeight)
+fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(8, 3))
 
-# Opens the image to be searched
-searchImage = Image.open("test_file")
-# Opens image to find template from
-templateImage = template_crop('test_file')
+ax1.imshow(coin, origin='lower')
+ax1.set_axis_off()
+ax1.set_title('template')
 
+ax2.imshow(image, origin='lower')
+ax2.set_axis_off()
+ax2.set_title('image')
+# highlight matched region
+hcoin, wcoin = coin.shape
+rect = plt.Rectangle((x, y), wcoin, hcoin, edgecolor='r', facecolor='none')
+ax2.add_patch(rect)
 
-# Calculates the amount of time the computation took
-t1=datetime.datetime.now()
-matchTemplate(searchImage, templateImage)
-delta=datetime.datetime.now()-t1
-print "Time=%d.%d"%(delta.seconds,delta.microseconds)
-print "end"
+ax3.imshow(result, origin='lower')
+ax3.set_axis_off()
+ax3.set_title('`match_template`\nresult')
+# highlight matched region
+ax3.autoscale(False)
+ax3.plot(x, y, 'o', markeredgecolor='r', markerfacecolor='none', markersize=10)
+
+plt.show()
+
