1dfa624cbee152665c21b389b59ad49ef7be155f
Patrick Korth <pkorth@umich.edu>
2012-06-22 08:34:40 -0400

    Initial commit; basic functionality.

---
+++ predictor.py
@@ -0,0 +1,13 @@
+
+class Predictor:
+
+    def init(self,stack):
+        #Perform initial analysis on the first frame if necessary and optionally
+        #return a shot-in-the-dark prediciton
+        pass
+
+    def predict(self,stack,data):
+        #Using the current and previous frame and previous ground-truth point
+        #positions, predict and return the position of the point in the current
+        #frame as well as a confidence
+        pass
