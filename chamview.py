aab75d6daa7c29fa2f94aad858c6ee438bfa335c
Manuel <medosal@miners.utep.edu>
2013-05-30 11:59:55 -0400

    Change to use only kinematic and guess predictors to reduce computation time

--- chamview.py
+++ chamview.py
@@ -99,8 +99,8 @@ def run(argDir,argChooser,argPreproc,argOutput,argPKind,argPPos):
     predictor,predictor_name = vocab.getPredictors()
     
     #Picking only some predictors for debugging purposes------------------------
-    predictor= [predictor[0], predictor[1], predictor[3]]
-    predictor_name= [predictor_name[0], predictor_name[1], predictor_name[3]]
+    predictor= [predictor[1], predictor[3]]
+    predictor_name= [predictor_name[1], predictor_name[3]]
     #---------------------------------------------------------------------------
 
     #Preprocess the ImageStack image
