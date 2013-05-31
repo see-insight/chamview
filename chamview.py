04d868087fa2583805812da09806e0e23c35ee43
Manuel <medosal@miners.utep.edu>
2013-05-31 11:05:50 -0400

    Turn off predictors

--- chamview.py
+++ chamview.py
@@ -99,8 +99,8 @@ def run(argDir,argChooser,argPreproc,argOutput,argPKind,argPPos):
     predictor,predictor_name = vocab.getPredictors()
     
     #Picking only some predictors for debugging purposes------------------------
-    predictor= [predictor[1], predictor[3]]
-    predictor_name= [predictor_name[1], predictor_name[3]]
+    predictor= []
+    predictor_name= []
     #---------------------------------------------------------------------------
 
     #Preprocess the ImageStack image
