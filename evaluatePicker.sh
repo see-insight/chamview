5a41fafd8045ff85c179e68a61372b111a761000
Manuel <medosal@miners.utep.edu>
2013-06-04 17:41:06 -0400

    Change the dataset to evaluate to a new with 110 frames

--- evaluatePicker.sh
+++ evaluatePicker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
-./chamview.py -c Performance -d ./images/ChamB.2 -p ./images/ChamB.2/gtdataB.txt
+
+#Dataset with 13 images
+#./chamview.py -c Performance -d ./images/ChamB.2 -p ./images/ChamB.2/gtdataB.txt
+
+#First dataset
+./chamview.py -c Performance -d ./dataSets/ChamB_LB/frames -p ./dataSets/ChamB_LB/manualpoints/2013_06_04_dosalman/points.txt
 
 
 
