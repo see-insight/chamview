0525d3effc11e286da528b708da0ba0d41809dd9
Manuel <medosal@miners.utep.edu>
2013-06-05 18:08:17 -0400

    Change the evaluation to only evaluate one predictor

--- evaluatePicker.sh
+++ evaluatePicker.sh
@@ -4,7 +4,7 @@
 #./chamview.py -c Performance -d ./images/ChamB.2 -p ./images/ChamB.2/gtdataB.txt
 
 #First dataset
-./chamview.py -c Performance -d ./dataSets/ChamB_LB/frames -p ./dataSets/ChamB_LB/manualpoints/2013_06_04_dosalman/points.txt
+./chamview.py -c Performance -r Kinematic -d ./dataSets/ChamB_LB/frames -p ./dataSets/ChamB_LB/manualpoints/2013_06_04_dosalman/points.txt
 
 
 
