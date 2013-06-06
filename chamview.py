9d9ed69cd3eeb5291115817d6b1dba4c424f0b83
Manuel <medosal@miners.utep.edu>
2013-06-06 11:41:03 -0400

    Select only some predictors

--- chamview.py
+++ chamview.py
@@ -113,6 +113,11 @@ def run(argDir,argChooser,argPreproc,argOutput,argPKind,argPPos,argSysInspector,
     #Load the Predictor subclass instances
     predictor,predictor_name = vocab.getPredictors()
     
+    #Choose only some predictors for debugging purposes-------------------------
+    predictor = [predictor[0], predictor[1], predictor[3]]
+    predictor_name = [predictor_name[0], predictor_name[1], predictor_name[3]]
+    #---------------------------------------------------------------------------
+    
     #Load the Predictor needed for user
     if pred != '':
         try:
