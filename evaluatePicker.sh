7b6bb5469ee35096fef481e8523abd8f13600b68
Manuel <medosal@miners.utep.edu>
2013-06-06 17:26:43 -0400

    evaluatePicker.sh deleted because we will add a .py file

--- evaluatePicker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-#Dataset with 13 images
-#./chamview.py -c Performance -d ./images/ChamB.2 -p ./images/ChamB.2/gtdataB.txt
-
-#First dataset
-./chamview.py -c Performance -r Kinematic -d ./dataSets/ChamB_LB/frames -p ./dataSets/ChamB_LB/manualpoints/2013_06_04_dosalman/points.txt
