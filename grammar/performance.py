d6ddb143a8efa40a2946f71920a5229ffd48873d
Manuel <medosal@miners.utep.edu>
2013-06-07 17:32:03 -0400

    Change the line of plots

--- grammar/performance.py
+++ grammar/performance.py
@@ -39,7 +39,6 @@ class Performance(Chooser):
         
         self.filledLists = False
         self.numImagesTested = 0 #Keeps track of the number of images tested
-        self.className = 'Performance' #The name of this class
         self.upperB =  50 #Max number of pixels that we care about for error
         self.tpBound = 5 #Bound to split True Positives and False Negatives
         self.numPlots = 0 #Determine the number of plots showed
@@ -199,7 +198,7 @@ class Performance(Chooser):
                 self.fo.write('  ' + str(xPlot[j]).zfill(4)+','+str(yPlot[j])+' %\n')
                         
             #Plot the error in the subplot
-            plt.plot(xPlot,yPlot, lw = 2)
+            plt.plot(xPlot,yPlot, lw = 1)
             
         title('Percentage of Error\n(For a given error from 1 to ' + 
               str(self.upperB) + ' pixels,\nthe next graph shows the percentage of ' +
@@ -284,7 +283,7 @@ class Performance(Chooser):
             #Plot the error in the subplot
             #n, bins, patches = plt.hist(yPlot, len(yPlot), normed=1, facecolor='g', alpha=0.75)
             #plt.plot(self.errorKindX[i], yPlot, 'bo', self.errorKindX[i], yPlot, 'k')
-            plt.plot(self.errorKindX[i],yPlot, lw = 2)
+            plt.plot(self.errorKindX[i],yPlot, lw = 1)
             
         title('Error on Prediction\nThis graph shows errors less or equal than '
                +str(self.upperB)+' pixels')
@@ -328,7 +327,7 @@ class Performance(Chooser):
                     self.fo.write('   ' + str(self.x[i][j]).zfill(4)+','+str(yVal)+' px\n')
             
                 #Plot the error in the subplot
-                plt.plot(self.x[i],yPlot, lw = 2)
+                plt.plot(self.x[i],yPlot, lw = 1)
             
             title('Point Kind: ' + self.pointKList[pointK]+'\nThis graph shows errors less or equal than '
                   +str(self.upperB)+' pixels')
@@ -369,7 +368,7 @@ class Performance(Chooser):
                 self.fo.write('  ' + str(self.x[i][j]).zfill(4)+','+str(yPlot[j])+'\n')
             
             #Plot the error in the subplot
-            plt.plot(self.x[i], yPlot, lw = 2)
+            plt.plot(self.x[i], yPlot, lw = 1)
             
         title('Accuracy on Prediction\n'
               + 'It is a number between 0 and 1. Close to 1 means good prediction')
@@ -420,7 +419,7 @@ class Performance(Chooser):
                 self.fo.write(' ' + str(self.x[i][j]).zfill(4)+','+str(yPlot[j])+'\n')
             
             #Plot the error in the subplot
-            plt.plot(self.x[i], yPlot, lw = 2)
+            plt.plot(self.x[i], yPlot, lw = 1)
             
         title('Accuracy and Confidence on Prediction')
         xlabel('Frame')
@@ -484,7 +483,7 @@ class Performance(Chooser):
             yPlot[len(yPlot)-1] = 1
                                          
             #Plot the error
-            plt.plot(xPlot, yPlot, lw = 2)
+            plt.plot(xPlot, yPlot, lw = 1)
             
         title('Receiver Operating Characteristic (ROC) Curve\n'+
               'A predictor is better if its curve is above other')
