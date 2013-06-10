058dc5651b030a7a7123b6f1cd11874224d50ce3
Manuel <medosal@miners.utep.edu>
2013-06-10 17:28:11 -0400

    Class that receives a text file and plot its information about errors on predictors

---
+++ plotPerformance.py
@@ -0,0 +1,16 @@
+#This class takes a text file and graphs errors on predictors
+#Manuel E. Dosal
+#June 10, 2013
+
+class PlotData:
+    
+    def __init__(self,directory=''):
+        self.numberPlots = 0
+        
+        self.get_textFile(directory)
+        
+    def get_textFile(self, directory):
+        pass
+        
+    def plot(self):
+        pass
