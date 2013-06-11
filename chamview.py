332e89b0e39c01657bf6479bfe871665627c3263
Manuel <medosal@miners.utep.edu>
2013-06-11 12:30:30 -0400

    Turn on only guess and kinematic predictors

--- chamview.py
+++ chamview.py
@@ -135,8 +135,8 @@ def run(argDir,argChooser,argPreproc,argOutput,argPKind,argPPos,argSysInspector,
     #Picking only some predictors for debugging purposes------------------------
     
     
-    predictor = [predictor[1], predictor[3], predictor[0]]
-    predictor_name = [predictor_name[1], predictor_name[3], predictor_name[0]]
+    predictor = [predictor[1], predictor[3]]#, predictor[0]]
+    predictor_name = [predictor_name[1], predictor_name[3]]#, predictor_name[0]]
     
     #---------------------------------------------------------------------------
 
