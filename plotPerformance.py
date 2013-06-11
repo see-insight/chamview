d5c586a0d8121fa72b6d631220a51e0d971d39e1
Manuel <medosal@miners.utep.edu>
2013-06-11 12:31:25 -0400

    Create methods used to plot data from a text file

--- plotPerformance.py
+++ plotPerformance.py
@@ -2,15 +2,50 @@
 #Manuel E. Dosal
 #June 10, 2013
 
+import os
+import vocabulary as vocab
+
 class PlotData:
     
     def __init__(self,directory=''):
+
+        #Attributes
         self.numberPlots = 0
+        self.file_in = None
+        self.graphNames = []
+        self.division = ''
         
-        self.get_textFile(directory)
+        #Get performance attributes
+        self.getPerformanceAtt()
         
-    def get_textFile(self, directory):
-        pass
+        #Get the dataset and plot
+        self.readAndPlot(directory)
         
-    def plot(self):
-        pass
+    def readAndPlot(self, filename):
+        
+        print 'graphNames: ', self.graphNames
+        
+        if os.path.exists(filename) == False: return
+        
+        self.file_in = open(filename)
+        for line in self.file_in:
+            if line in (self.graphNames):
+                self.plotGraph(line)
+        
+    def plotGraph(self,graphName):
+        
+        for line in self.file_in:
+            if line == self.division: break
+            
+            
+            
+            print line    
+        
+    def getPerformanceAtt(self):
+        #Create a new performance object
+        per = vocab.getChooser('Performance')
+        per.setup()
+        
+        #Update global variables
+        self.graphNames = per.graphNames
+        self.division = per.division
