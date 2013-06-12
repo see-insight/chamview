3dab9f8863ae2dfbe6210107e275f778c261a0e6
Manuel <medosal@miners.utep.edu>
2013-06-12 15:21:25 -0400

    Deleted printing messages

--- grammar/kinematic.py
+++ grammar/kinematic.py
@@ -93,8 +93,7 @@ class Kinematic(Predictor):
 			return result
 			
 		else:
-			#print 'YOU NEED AT LEAST 3 FRAMES OF POINTS TO PREDICT'
-			#print ' GO TO FRAME 4 AND THEN PRESS PREDICT!!! '
+			#Need at least 3 frames of points to predict
                         #Return zeros to avoid getting nan values
 			return zeros([stack.point_kinds,3])
 		
