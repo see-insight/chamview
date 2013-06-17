e2af07a57e93cb0c59f05629e3dd6e76c9f24284
Aaron Beckett <becketta@msu.edu>
2013-06-17 12:26:59 -0400

    changed update script

--- update.py
+++ update.py
@@ -8,6 +8,5 @@ import os
 import subprocess
 
 subprocess.call(['git','pull','--all'])
-if not os.path.isfile(os.getcwd()+os.path.sep+'.gitmodules'):
-    subprocess.call(['git','submodule','init'])
+subprocess.call(['git','submodule','init'])
 subprocess.call(['git','submodule','update'])
