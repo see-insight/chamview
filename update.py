d09119566ab7c7adcfb68c2fd5e1da3a2d46b102
Aaron Beckett <becketta@msu.edu>
2013-06-17 12:00:37 -0400

    Edited update file

--- update.py
+++ update.py
@@ -4,7 +4,10 @@
 #   Created by Aaron Beckett 06/17/2013
 #
 
+import os
 import subprocess
 
 subprocess.call(['git','pull','--all'])
+if not os.path.isfile(os.getcwd()+os.path.sep+'.gitmodules'):
+    subprocess.call(['git','submodule','init'])
 subprocess.call(['git','submodule','update'])
